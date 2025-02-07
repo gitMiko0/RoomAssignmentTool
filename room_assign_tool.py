8e7fbe43ceb03a03a4948b71b046bfc297c9e220
gitMiko0 <dustinbengo@gmail.com>
2025-02-07 01:21:14 -0700

    fixed time implementation

--- room_assign_tool.py
+++ room_assign_tool.py
@@ -6,16 +6,14 @@ from output_writer import write_output
 # To use this tool, input python room_assign_tool.py <dir of input.csv> <dir of output.csv> in the terminal (while in the project directory)
 def main():
     rooms, groups = load_input_data()
-    
     assignments = backtrack(groups, rooms, {})
     
-    if assignments:
-        # Print to terminal
+    if assignments:     # Solution found
         print("\nAssignments:")
-        write_output(filename=None, assignments=assignments)
-        write_output("assignments.csv", assignments)
+        write_output(filename=None, assignments=assignments)    # Print to terminal
+        write_output("assignments.csv", assignments)            # Save to file
         print("Assignments saved to assignments.csv")
-    else:
+    else:               # No solution for given input
         print("Error: Constraints cannot be satisfied with the provided input.")
 
 if __name__ == "__main__":
